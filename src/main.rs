18c2e0942c2f449bc725666dcda728d2fbdaea1e
Guewen Baconnier <guewen@gmail.com>
2016-11-12 23:02:18 +0100

    First working version
    Send an email to each person in the list.
    What could be improved:
    
    * CLI argument parsing
    * More tests
    * Better code (especially the huge main)
    * Different outputs with common traits (stdout, email, files)
    * Review memory handling

--- src/main.rs
+++ src/main.rs
@@ -1,181 +1,113 @@
 extern crate rand;
-use std::collections::HashSet;
-use std::hash::{Hash, Hasher};
-use rand::{thread_rng, Rng};
+extern crate lettre;
+extern crate yaml_rust;
 
-#[derive(Debug,Clone)]
-pub struct Person<'a> {
-    email: &'a str,
-    name: &'a str,
-    exclude: Vec<&'a str>, // list of emails
-}
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::BTreeMap;
 
-impl<'a> Person<'a> {
-    fn new(email: &'a str, name: &'a str, exclude: Vec<&'a str>) -> Person<'a> {
-        Person {
-            email: email,
-            name: name,
-            exclude: exclude,
-        }
-    }
+use yaml_rust::yaml;
 
-    fn can_give_to(&self, receiver: &'a str) -> bool {
-        !self.exclude.contains(&receiver)
-    }
-}
+mod email;
+mod pairs;
 
-impl<'a> PartialEq for Person<'a> {
-    fn eq(&self, other: &Person) -> bool {
-        self.email == other.email
-    }
-}
-
-impl<'a> Eq for Person<'a> { }
-
-
-impl<'a> Hash for Person<'a> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.email.hash(state);
-    }
-}
-
-
-#[derive(Debug,PartialEq)]
-pub struct Pair<'a> {
-    giver: &'a str,
-    receiver: &'a str,
-}
-
-impl<'a> Pair<'a> {
-    fn new(giver: &'a str, receiver: &'a str) -> Pair<'a> {
-        Pair {
-            giver: giver,
-            receiver: receiver,
-        }
-    }
-}
-
-
-#[derive(Debug)]
-pub struct Pool<'a> {
-    people: Vec<Person<'a>>,
-}
-
-impl<'a> Pool<'a> {
-    fn new(people: Vec<Person<'a>>) -> Pool<'a> {
-        Pool { people: people }
-    }
-
-    fn make_pairs(&self) -> Vec<Pair> {
-        let mut rng = thread_rng();
-        let mut pairs: Vec<Pair> = vec![];
-        let mut restart_distribution = false;
-        let mut tentatives = 0;
-        loop {  // until we find a correct distribution
-            tentatives += 1;
-            if tentatives > 1000 {
-                panic!("could not make pairs, probably due to recursive exclusions");
-            }
-            pairs.clear();
-            let mut people = self.people.clone();
-            let people_emails: HashSet<&str> = people.iter().map(|ref p| p.email).collect();
-            let mut consumed: Vec<&Person> = vec![];
-            rng.shuffle(&mut people);
-            let mut givers = people.iter();
-            let mut receivers = people.iter().cycle();
-            loop {
-                if restart_distribution {
-                    restart_distribution = false;
-                    break;
-                }
-                let person = match givers.next() {
-                    Some(person) => person,
-                    None => break,
-                };
-                let consumed_emails: HashSet<&str> = consumed.iter().map(|ref p| p.email).collect();
-                let mut remaining: HashSet<&str> = people_emails.difference(&consumed_emails).cloned().collect();
-                remaining.remove(person.email);
-                let exclude: HashSet<&str> = person.exclude.iter().cloned().collect();
-                if remaining.is_subset(&exclude) {
-                    // The only remaining candidates are excluded for this person!
-                    // we start again the distribution
-                    restart_distribution = true;
-                    break;
-                } else {
-                    let receiver: &Person;
-                    loop {
-                         match receivers.next() {
-                            Some(r) => {
-                                if r != person && person.can_give_to(r.email) && !consumed.contains(&r) {
-                                    receiver = r;
-                                    break;
-                                }
-                            },
-                            None => unreachable!()
-                        }
-                    }
-                    consumed.push(receiver);
-                    let pair = Pair::new(person.email, receiver.email);
-                    pairs.push(pair);
-                }
-            }
-            if consumed.len() == people.len() {
-                break;
-            }
-        }
-
-        pairs
-    }
-}
 
 fn main() {
-    let pool = Pool::new(vec![
-        Person::new("a@example.com", "A", vec!["b@example.com"]),
-        Person::new("b@example.com", "B", vec![]),
-        Person::new("c@example.com", "C", vec![]),
-        Person::new("d@example.com", "D", vec!["b@example.com", "c@example.com"]),
-        Person::new("e@example.com", "E", vec![]),
-        Person::new("f@example.com", "F", vec![]),
-    ]);
+    let args: Vec<_> = env::args().collect();
+    let mut file = File::open(&args[1]).unwrap();
+    let mut content = String::new();
+    file.read_to_string(&mut content).unwrap();
+
+    let docs = yaml::YamlLoader::load_from_str(&content).unwrap();
+    let doc = &docs[0];
+    let people = match doc["people"] {
+        yaml::Yaml::Array(ref r) => r,
+        yaml::Yaml::BadValue => panic!("No people in the yaml file"),
+        _ => panic!("Wrong format of people")
+    };
+    let mut all_people = vec!();
+    println!("Computing pairs...");
+    for person in people.iter() {
+        let name = match person["name"] {
+            yaml::Yaml::String(ref r) => r,
+            yaml::Yaml::BadValue => panic!("Missing 'name' for a person {:?}", person),
+            _ => panic!("Wrong format of 'name' for a person {:?}", person)
+        };
+        let email = match person["email"] {
+            yaml::Yaml::String(ref r) => r,
+            yaml::Yaml::BadValue => panic!("Missing 'email' for a person {:?}", person),
+            _ => panic!("Wrong format of 'email' for a person {:?}", person)
+        };
+        let exclude = match person["exclude"] {
+            yaml::Yaml::Array(ref r) => r.iter().map(|s| s.as_str().unwrap()).collect(),
+            yaml::Yaml::Null => vec!(),
+            yaml::Yaml::BadValue => vec!(),
+            _ => panic!("Wrong format of 'exclude' for a person {:?}", person)
+        };
+        let person = pairs::Person::new(email, name, exclude);
+        all_people.push(person);
+    }
+    let mut pool = pairs::Pool::new(all_people.iter().collect::<Vec<&pairs::Person>>());
     let pairs = pool.make_pairs();
+    println!("Pairs generated");
 
-    println!("{:?}", pairs);
 
-}
+    let config: BTreeMap<&str, &yaml_rust::Yaml> = match doc["config"] {
+        yaml::Yaml::Hash(ref r) => r.iter().map(|(k, v)| (k.as_str().unwrap(), v)).collect(),
+        yaml::Yaml::BadValue => panic!("No 'config' in the yaml file"),
+        _ => panic!("Wrong format of 'config'")
+    };
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    let smtp_config: BTreeMap<&str, &yaml_rust::Yaml> = match config["smtp"] {
+        &yaml::Yaml::Hash(ref r) => r.iter().map(|(k, v)| (k.as_str().unwrap(), v)).collect(),
+        &yaml::Yaml::BadValue => panic!("No 'config→smtp' in the yaml file"),
+        _ => panic!("Wrong format of 'config→smtp'")
+    };
+    let host = match smtp_config["host"] {
+        &yaml::Yaml::String(ref r) => r,
+        _ => panic!("Wrong format or missing 'config→smtp→host'")
+    };
+    let port = match smtp_config["port"] {
+        &yaml::Yaml::Integer(ref r) => r,
+        _ => panic!("Wrong format or missing 'config→smtp→port'")
+    };
+    let user = match smtp_config["user"] {
+        &yaml::Yaml::String(ref r) => r,
+        _ => panic!("Wrong format or missing 'config→smtp→user'")
+    };
+    let password = match smtp_config["password"] {
+        &yaml::Yaml::String(ref r) => r,
+        _ => panic!("Wrong format or missing 'config→smtp→password'")
+    };
 
-    #[test]
-    fn exclusion() {
-        let a = Person::new("a@example.com", "A", vec!["b@example.com"]);
-        let b = Person::new("b@example.com", "B", vec![]);
-        assert!(a.can_give_to(b.email) == false);
-        assert!(b.can_give_to(a.email) == true);
-    }
+    let email_server = email::EmailServer::new(host, *port as u16, user, password);
 
-    #[test]
-    fn pool() {
-        let pool = Pool::new(vec![
-            Person::new("a@example.com", "A", vec!["b@example.com"]),
-            Person::new("b@example.com", "B", vec![]),
-            Person::new("c@example.com", "C", vec![]),
-            Person::new("d@example.com", "D", vec!["b@example.com", "c@example.com"]),
-        ]);
-        let pairs: Vec<Pair> = vec![];
-        assert!(pool.make_pairs() == pairs);
-    }
+    let email_config: BTreeMap<&str, &yaml_rust::Yaml> = match config["email"] {
+        &yaml::Yaml::Hash(ref r) => r.iter().map(|(k, v)| (k.as_str().unwrap(), v)).collect(),
+        &yaml::Yaml::BadValue => panic!("No 'config→email' in the yaml file"),
+        _ => panic!("Wrong format of 'config→email'")
+    };
+    let from = match email_config["from"] {
+        &yaml::Yaml::String(ref r) => r,
+        _ => panic!("Wrong format or missing 'config→email→from'")
+    };
+    let subject = match email_config["subject"] {
+        &yaml::Yaml::String(ref r) => r,
+        _ => panic!("Wrong format or missing 'config→email→subject'")
+    };
+    let body = match email_config["body"] {
+        &yaml::Yaml::String(ref r) => r,
+        _ => panic!("Wrong format or missing 'config→email→body'")
+    };
+    let email_template = email::EmailTemplate::new(from, subject, body);
 
-    #[test]
-    fn pair_equality() {
-        let pair1 = Pair::new("a@example.com", "b@example.com");
-        let pair2 = Pair::new("a@example.com", "b@example.com");
-        let pair3 = Pair::new("a@example.com", "c@example.com");
-        let pair4 = Pair::new("b@example.com", "a@example.com");
-        assert!(pair1 == pair2);
-        assert!(pair2 == pair1);
-        assert!(pair1 != pair3);
-        assert!(pair1 != pair4);
-    }
+    println!("Sending emails");
+    email::send_emails(&email_server, &email_template, &pairs);
+    println!("Done!");
+
+    // for pair in pairs.iter() {
+    //     println!("{} → {}", pair.giver.email, pair.receiver.email);
+    // }
+
 }
---
+++ src/pairs.rs
@@ -0,0 +1,168 @@
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+use rand::{thread_rng, Rng};
+
+#[derive(Debug,Clone)]
+pub struct Person<'a> {
+    pub email: &'a str,
+    pub name: &'a str,
+    pub exclude: Vec<&'a str>, // list of emails
+}
+
+impl<'a> Person<'a> {
+    pub fn new(email: &'a str, name: &'a str, exclude: Vec<&'a str>) -> Person<'a> {
+        Person {
+            email: email,
+            name: name,
+            exclude: exclude,
+        }
+    }
+
+    pub fn can_give_to(&self, receiver: &'a str) -> bool {
+        !self.exclude.contains(&receiver)
+    }
+}
+
+impl<'a> PartialEq for Person<'a> {
+    fn eq(&self, other: &Person) -> bool {
+        self.email == other.email
+    }
+}
+
+impl<'a> Eq for Person<'a> { }
+
+
+impl<'a> Hash for Person<'a> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.email.hash(state);
+    }
+}
+
+
+#[derive(Debug,PartialEq)]
+pub struct Pair<'a> {
+    pub giver: &'a Person<'a>,
+    pub receiver: &'a Person<'a>,
+}
+
+impl<'a> Pair<'a> {
+    pub fn new(giver: &'a Person<'a>, receiver: &'a Person<'a>) -> Pair<'a> {
+        Pair {
+            giver: giver,
+            receiver: receiver,
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Pool<'a> {
+    people: Vec<&'a Person<'a>>,
+}
+
+impl<'a> Pool<'a> {
+    pub fn new(people: Vec<&'a Person<'a>>) -> Pool<'a> {
+        Pool { people: people }
+    }
+
+    pub fn make_pairs(&mut self) -> Vec<Pair> {
+        let mut rng = thread_rng();
+        let mut pairs: Vec<Pair> = vec![];
+        let mut restart_distribution = false;
+        let mut tentatives = 0;
+        loop {  // until we find a correct distribution
+            tentatives += 1;
+            if tentatives > 1000 {
+                panic!("could not make pairs, probably due to recursive exclusions");
+            }
+            pairs.clear();
+            let mut people = self.people.clone();
+            let people_emails: HashSet<&str> = self.people.iter().map(|ref p| p.email).collect();
+            let mut consumed: Vec<&Person> = vec![];
+            rng.shuffle(&mut people);
+            let mut givers = people.iter();
+            let mut receivers = people.iter().cycle();
+            loop {
+                if restart_distribution {
+                    restart_distribution = false;
+                    break;
+                }
+                let person = match givers.next() {
+                    Some(person) => person,
+                    None => break,
+                };
+                let consumed_emails: HashSet<&str> = consumed.iter().map(|ref p| p.email).collect();
+                let mut remaining: HashSet<&str> = people_emails.difference(&consumed_emails).cloned().collect();
+                remaining.remove(person.email);
+                let exclude: HashSet<&str> = person.exclude.iter().cloned().collect();
+                if remaining.is_subset(&exclude) {
+                    // The only remaining candidates are excluded for this person!
+                    // we start again the distribution
+                    restart_distribution = true;
+                    break;
+                } else {
+                    let receiver: &Person;
+                    loop {
+                         match receivers.next() {
+                            Some(r) => {
+                                if r != person && person.can_give_to(r.email) && !consumed.contains(&r) {
+                                    receiver = r;
+                                    break;
+                                }
+                            },
+                            None => unreachable!()
+                        }
+                    }
+                    consumed.push(receiver);
+                    let pair = Pair::new(&person, &receiver);
+                    pairs.push(pair);
+                }
+            }
+            if consumed.len() == people.len() {
+                break;
+            }
+        }
+
+        pairs
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exclusion() {
+        let a = Person::new("a@example.com", "A", vec!["b@example.com"]);
+        let b = Person::new("b@example.com", "B", vec![]);
+        assert!(a.can_give_to(b.email) == false);
+        assert!(b.can_give_to(a.email) == true);
+    }
+
+    #[test]
+    fn pool() {
+        let p1 = Person::new("a@example.com", "A", vec!["b@example.com"]);
+        let p2 = Person::new("b@example.com", "B", vec![]);
+        let p3 = Person::new("c@example.com", "C", vec![]);
+        let p4 = Person::new("d@example.com", "D", vec!["b@example.com", "c@example.com"]);
+        let pool = Pool::new(vec![p1, p2, p3, p4]);
+        let pairs: Vec<Pair> = vec![];
+        assert!(pool.make_pairs() == pairs);
+    }
+
+    #[test]
+    fn pair_equality() {
+        let p1 = Person::new("a@example.com", "A", vec!["b@example.com"]);
+        let p2 = Person::new("b@example.com", "B", vec![]);
+        let p3 = Person::new("c@example.com", "C", vec![]);
+        let pair1 = Pair::new(p1, p2);
+        let pair2 = Pair::new(p1, p2);
+        let pair3 = Pair::new(p1, p3);
+        let pair4 = Pair::new(p2, p1);
+        assert!(pair1 == pair2);
+        assert!(pair2 == pair1);
+        assert!(pair1 != pair3);
+        assert!(pair1 != pair4);
+    }
+}

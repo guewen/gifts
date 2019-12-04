d2ed604e2657516d91a5ad13802f84da66117792
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-12-04 22:06:49 +0100

    Add debug mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate clap;
 extern crate lettre;
 extern crate lettre_email;
 extern crate rand;
@@ -8,31 +9,52 @@ use std::env;
 use std::fs::File;
 use std::io::{self, prelude::*};
 
-// use serde_yaml;
+use clap::{App, Arg};
 
 mod config;
 mod email;
 mod pairs;
 
 fn main() -> io::Result<()> {
+    let matches = App::new("Gifts!")
+        .version("0.1.0")
+        .author("Guewen Baconnier <guewen@gmail.com>")
+        .about("Send Secret Emails for Secret-Santa-style events")
+        .arg(
+            Arg::with_name("config")
+                .index(1)
+                .takes_value(true)
+                .required(true)
+                .help("Configuration File"),
+        )
+        .arg(
+            Arg::with_name("debug")
+                .short("d")
+                .long("debug")
+                .help("Do not send emails, show dummy results."),
+        )
+        .get_matches();
+
     let args: Vec<_> = env::args().collect();
     let mut file = File::open(&args[1])?;
     let mut content = String::new();
     file.read_to_string(&mut content)?;
 
     let config_file: config::ConfigFile = serde_yaml::from_str(&content).unwrap();
-    println!("{:#?}", config_file);
     println!("Computing pairs...");
     let mut pool = pairs::Pool::new(config_file.people);
     let pairs = pool.make_pairs();
     println!("Pairs generated");
 
-    println!("Sending emails");
-    email::send_emails(&config_file.config.smtp, &config_file.config.email, &pairs);
+    if matches.is_present("debug") {
+        for pair in pairs.iter() {
+            println!("{} → {}", pair.giver.email, pair.receiver.email);
+        }
+    } else {
+        println!("Sending emails");
+        email::send_emails(&config_file.config.smtp, &config_file.config.email, &pairs);
+    }
     println!("Done!");
 
-    // for pair in pairs.iter() {
-    //     println!("{} → {}", pair.giver.email, pair.receiver.email);
-    // }
     Ok(())
 }
